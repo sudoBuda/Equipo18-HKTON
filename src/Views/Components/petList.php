c5e17904abdeb4e84001fcbb1a76fabd3a7f3d26
maximiliano <maxlobosc@gmail.com>
2022-07-21 19:04:40 +0200

    popup

--- src/Views/Components/petList.php
+++ src/Views/Components/petList.php
@@ -21,15 +21,21 @@
             foreach ($data ["pet"] as $pet) {
                echo <<< TAG
 
-                    <div div class="card pet_card" style="width: 18rem;" id="open_details">
+               <div class="card pet_card" style="width: 18rem;" id="open_details">
                     <img  src="{$pet->getImage()}" class="card-img-top image_pet"  alt="...">
                     <div class="card-body">
+
                         <h5 class="card-title pet_name">{$pet->getName()}</h5>
                         <p class="card-text pet_decription">{$pet->getDescription()}</p>
-                        <a href="mailto:{$pet->getContact()}"<p>{$pet->getContact()}</p></a>
-                        <a href="#" class="btn btn-primary btn_favorite"><i class="bi bi-house-heart-fill icon_favorite "></i>Favorite</a>
+                       
+                        <a  class="btn btn-primary btn_favorite"><i class="bi bi-house-heart-fill icon_favorite "></i>Favorite</a>
+                        <button type="button" class="btn btn-primary" data-bs-toggle="popover" data-bs-trigger="focus" title="{$pet->getContact()}" data-bs-content="6XXX XX XX XX">📱✉️</button>     
                     </div>
-                    </div>
+             
+               </div> 
+ 
+             
+
                 
    
                TAG;
@@ -41,6 +47,11 @@
        <?php
          require_once("src/Views/Components/footer.php");
        ?>
-        <script src="js/function.js"></script>
+     <script>
+               var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+               var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+                return new bootstrap.Popover(popoverTriggerEl)
+})
+     </script>
     </body>
 </html>
