8499cac99127411896fcbdeb8134db34e996c3ef
sudoBuda <juanfranciscobalseca@gmail.com>
2022-07-21 15:51:32 +0200

    footer update

--- src/Views/Components/footer.php
+++ src/Views/Components/footer.php
@@ -1,34 +1,34 @@
-<footer>
-    <div class="grid-layout">
-        <div class="caja-logo">
-            <img class="logo-footer" src="public/images/logo.png" alt="logo DogCatAdoption" />
-        </div>
-        <div class="caja-horario">
-            <h3>HORARIO</h3>
-            <div class="horario">
-                <ul>
-                    <li>Lunes a Viernes 9h a 20h</li>
-                    <li>Sábado y Domingo 13h a 20h</li>
-                </ul>
+    <footer>
+        <div class="grid-layout">
+            <div class="caja-logo">
+                <img class="logo-footer" src="public/images/logo.png" alt="logo DogCatAdoption" />
+            </div>
+            <div class="caja-horario">
+                <h3 style="color: rgb(250, 48, 48);">HORARIO</h3>
+                <div class="horario">
+                    <ul>
+                        <li>Lunes a Viernes 9h a 20h</li>
+                        <li>Sábado y Domingo 13h a 20h</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="caja-mapa">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12153.900163777902!2d-3.6863441!3d40.398326!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xe1297343074f52c0!2sTalent%20Garden%20Madrid!5e0!3m2!1ses!2ses!4v1654043331088!5m2!1ses!2ses" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+            <div class="caja-siguenos">
+                <h3 style="color: rgb(250, 48, 48);">SIGUENOS</h3>
+                <div class="img-footer" style="padding-left: 5px;">
+                    <a href="https://www.facebook.com/">
+                        <img src="https://cdn-icons-png.flaticon.com/128/747/747374.png" alt="icono facebook"></a>
+                    <a href="https://www.instagram.com/">
+                        <img src="https://cdn-icons-png.flaticon.com/128/725/725339.png" alt="icono Instagram"></a>
+                </div>
+            </div>
+            <br>
+            <div class="caja-copyright">
+                <small>© G18 Hakaton 2022 - Todos los Derechos Reservados<br>
+                    c/ Juan de Mariana, 15 - 28035 Madrid
+                </small>
             </div>
         </div>
-        <div class="caja-mapa">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12153.900163777902!2d-3.6863441!3d40.398326!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xe1297343074f52c0!2sTalent%20Garden%20Madrid!5e0!3m2!1ses!2ses!4v1654043331088!5m2!1ses!2ses" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-        </div>
-        <div class="caja-siguenos">
-            <h3>SIGUENOS</h3>
-            <div class="img-footer">
-                <a href="https://www.facebook.com/">
-                    <img src="https://cdn-icons-png.flaticon.com/128/747/747374.png" alt="icono facebook" /></a>
-                <a href="https://www.instagram.com/">
-                    <img src="https://cdn-icons-png.flaticon.com/128/725/725339.png" alt="icono Instagram" /></a>
-            </div>
-        </div>
-        <br>
-        <div class="caja-copyright">
-            <small>© G18 Hakaton 2022 - Todos los Derechos Reservados<br>
-                c/ Juan de Mariana, 15 - 28035 Madrid
-            </small>
-        </div>
-    </div>
-</footer>
+    </footer>
