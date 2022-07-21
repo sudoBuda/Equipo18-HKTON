8a1a99756dff9654a8e122cf31be8af941ddb8fb
maximiliano <maxlobosc@gmail.com>
2022-07-21 10:18:47 +0200

    css container card

--- src/Views/Components/petList.php
+++ src/Views/Components/petList.php
@@ -12,24 +12,28 @@
        <main>
 
        <h2 class="subtitle">PETS AVIABLE FOR ADOPTION</h2>
-
-           <section class="row" >
+      
+      
+       <div class="container_cards">
+        
 
        <?php
             foreach ($data ["pet"] as $pet) {
                echo <<< TAG
-               <div class="container_cards">
-                    <div class="card pet_card" style="width: 18rem;" id="open_details">
-                    <img src="{$pet->getImage()}" class="card-img-top" alt="...">
+
+                    <div div class="card pet_card" style="width: 18rem;" id="open_details">
+                    <img  src="{$pet->getImage()}" class="card-img-top image_pet"  alt="...">
                     <div class="card-body">
                         <h5 class="card-title pet_name">{$pet->getName()}</h5>
                         <p class="card-text pet_decription">{$pet->getDescription()}</p>
                         <a href="#" class="btn btn-primary btn_favorite"><i class="bi bi-house-heart-fill icon_favorite "></i>Favorite</a>
                     </div>
-                </div>
+                    </div>
+                
    
                TAG;
             } ?>
+       </div>
        
        </main>
        
